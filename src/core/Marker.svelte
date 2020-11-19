5645bf589408bdfcf8410ecbbf79fa2635b56aaa
kleem <clara.bacciu@gmail.com>
2020-11-19 20:08:19 +0100

    Closes #33

--- src/core/Marker.svelte
+++ src/core/Marker.svelte
@@ -8,14 +8,16 @@
 
 
 @media only screen and (max-width: 600px) {
-    .marker {
-        transform: scale(1.5);
+    .marker_content {
+        transform: scale(2);
     }
 }
 </style>
 
 {#if position.layers.has($current_layer.name)}
 <g class="selectable marker" transform="translate({position.x},{position.y}) scale({$semantic_zoom})" on:click>
-    <slot></slot>
+    <g class="marker_content">
+        <slot></slot>
+    </g>
 </g>
 {/if}
