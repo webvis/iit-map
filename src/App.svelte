d062a56f01fe77d7308e1a96f08f445bf10ddf43
nitaku <matteo.abrate@gmail.com>
2021-02-23 19:13:34 +0100

    anymapper reference updated in footer

--- src/App.svelte
+++ src/App.svelte
@@ -176,7 +176,7 @@
 
 <img src="assets/IIT+CNR-RGB-logos.svg" alt="IIT-CNR logo" class="logo"/>
 
-<footer><a href="https://www.iit.cnr.it/privacy-policy/">Privacy Policy</a> - Made with <a href="https://github.com/webvis/iit-map">MapVis</a>, by Human Centered Technologies Lab @IIT-CNR</footer>
+<footer><a href="https://www.iit.cnr.it/privacy-policy/">Privacy Policy</a> - Made with <a href="https://github.com/webvis/anymapper">anymapper</a>, by Human Centered Technologies Lab @IIT-CNR</footer>
 
 <OmniBox on:search={handleSearch}>
 	<ResultsBox>
