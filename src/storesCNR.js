aacdcdf11d3408a667d851e161e999d548b87969
nitaku <matteo.abrate@gmail.com>
2021-07-28 14:51:02 +0200

    lighter personnel photos

--- src/storesCNR.js
+++ src/storesCNR.js
@@ -106,5 +106,5 @@ export function getQualifica(person) {
 }
 
 export function getImmagine(person) {
-    return `https://www.iit.cnr.it/wp-content/themes/cnr/foto_personali/${person.immagine.replace("'",'')}`
+    return `https://www.iit.cnr.it/wp-content/themes/cnr/foto_personali_400/${person.immagine.replace("'",'')}`
 }
