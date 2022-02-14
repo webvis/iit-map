583ce31be0ee06852fd72e748a374939a5e9e45f
nitaku <matteo.abrate@gmail.com>
2022-02-14 11:33:04 +0100

    keyboard navigation of results + smui updated

--- src/CNRResults.svelte
+++ src/CNRResults.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { results } from 'anymapper'
+    import { results, select } from 'anymapper'
     import { getQualifica } from './storesCNR.js'
     
     import List, {Item, Text, Graphic} from '@smui/list'
@@ -23,26 +23,20 @@
 <List singleSelection class="cnr-results">
     {#each $results as r}
         {#if r.type == 'person'}
-            <a href="#{r.email}">
-                <Item>
-                    <Graphic class="material-icons">person</Graphic>
-                    <Text>{r.nome} {r.cognome} <span class="secondary">{getQualifica(r)}</span></Text>
-                </Item>
-            </a>
+            <Item on:SMUI:action={() => select(r.email)}>
+                <Graphic class="material-icons">person</Graphic>
+                <Text>{r.nome} {r.cognome} <span class="secondary">{getQualifica(r)}</span></Text>
+            </Item>
         {:else if r.type == 'office'}
-            <a href="#{r.id}">
-                <Item>
-                    <Graphic class="material-icons">meeting_room</Graphic>
-                    <Text>{r.id} <span class="secondary">Ufficio - Edificio {r.edificio}, Piano {r.piano}</span></Text>
-                </Item>
-            </a>
+            <Item on:SMUI:action={() => select(r.id)}>
+                <Graphic class="material-icons">meeting_room</Graphic>
+                <Text>{r.id} <span class="secondary">Ufficio - Edificio {r.edificio}, Piano {r.piano}</span></Text>
+            </Item>
         {:else if r.type == 'room'}
-            <a href="#{r.id}">
-                <Item>
-                    <Graphic class="material-icons">meeting_room</Graphic>
-                    <Text>{r.id} <span class="secondary">Stanza</span></Text>
-                </Item>
-            </a>
+            <Item on:SMUI:action={() => select(r.id)}>
+                <Graphic class="material-icons">meeting_room</Graphic>
+                <Text>{r.id} <span class="secondary">Stanza</span></Text>
+            </Item>
         {/if}
     {/each}
 </List>
