7e448d03b61fd9735ba5fde52cd2271eecaea3b7
nitaku <matteo.abrate@gmail.com>
2022-02-14 17:44:49 +0100

    shorter footer

--- src/App.svelte
+++ src/App.svelte
@@ -181,7 +181,7 @@
 
 <img src="assets/IIT+CNR-RGB-logos.svg" alt="IIT-CNR logo" class="logo"/>
 
-<footer><a href="https://www.iit.cnr.it/privacy-policy/">Privacy Policy</a> - <a href="credits">Credits</a> - Powered by <a href="https://github.com/webvis/anymapper">anymapper</a>, by <a href="//hct.iit.cnr.it/">Human Centered Technologies Lab</a> @<a href="//www.iit.cnr.it/">IIT-CNR</a></footer>
+<footer><a href="https://www.iit.cnr.it/privacy-policy/">Privacy</a> - <a href="credits">Credits</a> - Powered by <a href="https://github.com/webvis/anymapper">anymapper</a>, by <a href="//hct.iit.cnr.it/">HCT Lab</a> @<a href="//www.iit.cnr.it/">CNR-IIT</a></footer>
 
 <OmniBox on:search={handleSearch}>
 	<ResultsBox>
