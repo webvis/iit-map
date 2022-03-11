92eb3e6c688df5548fdaf8c20ac3cab902161dfc
nitaku <matteo.abrate@gmail.com>
2022-03-11 09:33:19 +0100

    fix for email an tel links

--- src/PersonInfo.svelte
+++ src/PersonInfo.svelte
@@ -9,9 +9,9 @@
 <Content>
     <table>
         <tr><td>web</td><td><a href="//{web}">{web}</a></td></tr>
-        <tr><td>e-mail</td><td><a href="mailto: {$selection.email}">{$selection.email}</a></td></tr>
-        {#if $selection.telefono}<tr><td>Telefono</td><td><a href="tel: {$selection.telefono}">{$selection.telefono}</a></td></tr>{/if}
-        {#if $selection.interno}<tr><td>Interno</td><td><a href="tel: {$selection.telefono}">{$selection.interno}</a></td></tr>{/if}
-        {#if $selection.cellulare}<tr><td>Cellulare</td><td><a href="tel: {$selection.cellulare}">{$selection.cellulare}</a></td></tr>{/if}
+        <tr><td>e-mail</td><td><a href="mailto:{$selection.email}">{$selection.email}</a></td></tr>
+        {#if $selection.telefono}<tr><td>Telefono</td><td><a href="tel:{$selection.telefono}">{$selection.telefono}</a></td></tr>{/if}
+        {#if $selection.interno}<tr><td>Interno</td><td><a href="tel:{$selection.telefono}">{$selection.interno}</a></td></tr>{/if}
+        {#if $selection.cellulare}<tr><td>Cellulare</td><td><a href="tel:{$selection.cellulare}">{$selection.cellulare}</a></td></tr>{/if}
     </table>
 </Content>
